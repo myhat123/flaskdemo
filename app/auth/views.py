96aac669e6d56ae75e622665f429beb33556667f
hjh0915 <623299157@qq.com>
2020-01-07 14:26:56 +0800

    增加验证码功能

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,9 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, make_response
+from flask import current_app
 from flask_login import login_required, logout_user, login_user
 from . import auth
 from ..models import User
+from .. import vcode
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,14 +15,32 @@ def login():
     if request.method == 'POST':
         username = request.form.get('fname')
         password = request.form.get('fpwd')
+        vcode = request.form.get('fcode')
 
         u = User.query.filter_by(username=username).first()
         if u == None:
             return render_template('auth/login.html')
         
-        if u.verify_password(password):
+        v = request.cookies.get('auth_code')
+
+        if u.verify_password(password) and v == vcode:
             login_user(u)
             return redirect('/main/show_rpt_sum_apart/20191127')
+        else:
+            return render_template('auth/login.html')
+
+@auth.route('/makeimage')
+def make_image():
+    """生成验证码"""
+
+    s = vcode.gen_rand_str()
+    c = vcode.make_image(current_app.config['IMAGE_FONT'], s)
+    rsp = make_response(c.getvalue())
+    rsp.mimetype = "image/jpeg"
+
+    rsp.set_cookie('auth_code', s)
+
+    return rsp
 
 @auth.route('/logout')
 @login_required
--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__)) #config文件的基准目录
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
+    IMAGE_FONT = os.path.join(basedir, './app/static/font/wqy-microhei.ttc')
 
     @staticmethod
     def init_app(app):
